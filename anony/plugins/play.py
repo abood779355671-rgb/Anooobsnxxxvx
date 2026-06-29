44c68937c12d98c3f5106c0108e66822dc1c00d5
abood779355671-rgb <abood779355671@gmail.com>
2026-06-29 20:45:32 +0300

    Upload via GitHub ZIP Uploader

--- anony/plugins/play.py
+++ anony/plugins/play.py
@@ -21,7 +21,7 @@ def playlist_to_queue(chat_id: int, tracks: list) -> str:
     return text
 
 @app.on_message(
-    cmd(["play", "playforce", "vplay", "vplayforce", "تشغيل", "شغل", "تشغيل_فيديو", "تشغيل_فوري"])
+    cmd(["play", "playforce", "vplay", "vplayforce", "تشغيل", "شغل", "تشغيل_فيديو", "تشغيل_فوري", "فيديو"])
     & filters.group
     & ~app.bl_users
 )
