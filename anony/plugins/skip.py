a4da60b3c252a031b9f7d94eebf667d5ba587e53
abood779355671-rgb <abood779355671@gmail.com>
2026-06-29 22:41:21 +0300

    Upload via GitHub ZIP Uploader

--- anony/plugins/skip.py
+++ anony/plugins/skip.py
@@ -2,10 +2,9 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-
 from pyrogram import filters, types
 
-from anony import anon, app, db, lang
+from anony import anon, app, db, lang, queue
 from anony.helpers import can_manage_vc_strict, cmd
 
 
@@ -16,5 +15,18 @@ async def _skip(_, m: types.Message):
     if not await db.get_call(m.chat.id):
         return await m.reply_text(m.lang["not_playing"])
 
+    delete_btn = types.InlineKeyboardMarkup(
+        [[types.InlineKeyboardButton(text=m.lang["delete_msg"], callback_data="delete_this")]]
+    )
+
+    # التحقق من وجود قائمة انتظار
+    has_next = queue.get_next(m.chat.id, check=True)
+
     await anon.play_next(m.chat.id)
-    await m.reply_text(m.lang["play_skipped"].format(m.from_user.mention))
+
+    if has_next:
+        text = m.lang["play_skipped"].format(m.from_user.mention)
+    else:
+        text = m.lang["play_skipped_empty"].format(m.from_user.mention)
+
+    await m.reply_text(text, reply_markup=delete_btn)
