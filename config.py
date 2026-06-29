456350b35efb97c5ce1f4993c0051f121305708a
abood779355671-rgb <abood779355671@gmail.com>
2026-06-29 21:28:30 +0300

    Upload via GitHub ZIP Uploader

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
 
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
         self.OWNER_ID = int(getenv("OWNER_ID", 0))
+        self.OWNER_USERNAME = getenv("OWNER_USERNAME", "")
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
